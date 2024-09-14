c9755e2fad4547c846ca1b082aa7eafd88aaa467
sike25 <oogieva25@amherst.edu>
2024-09-14 18:14:41 -0400

    some org

--- backend/custom_news.py
+++ backend/custom_news.py
@@ -13,9 +13,9 @@ fake_portfolio = [
     Asset(ticker="AMZN", sector="Technology"),
     Asset(ticker="AAPL", sector="Technology"),
     Asset(ticker="GOOG", sector="Technology"),
-    Asset(ticker="MRNA", sector="Healthcare"),
-    Asset(ticker="NVO",  sector="Healthcare"),
-    Asset(ticker="BAC",  sector="Financial"),
+    Asset(ticker="MSFT", sector="Technology"),
+    Asset(ticker="TSLA", sector="Technology"),
+    Asset(ticker="META", sector="Technology"),
     Asset(ticker="GS",   sector="Financial"),
 ]
 
@@ -57,21 +57,6 @@ def add_news_for_return(file_name, nb_files):
             news_to_return.append(new_news)
     print(f"Added {nb_files} files from {file_name} to results")
 
-def get_industries_in_portfolio(portfolio):
-    counts = [0 for i in range(5)]
-    for asset in portfolio:
-        if asset.sector == "Technology":
-            counts[0] += 1
-        elif asset.sector == "Healthcare":
-            counts[1] += 1
-        elif asset.sector == "Financial":
-            counts[2] += 1
-        elif asset.sector == "Basic Materials":
-            counts[3] += 1
-        else:
-            counts[4] += 1
-    return counts
-
 def get_tickers_in_portfolio(portfolio):
     tickers = []
     for asset in portfolio:
@@ -79,56 +64,27 @@ def get_tickers_in_portfolio(portfolio):
     return tickers
 
 # GENERAL NEWS
-
 general_news_url = f"https://stocknewsapi.com/api/v1/category?section=general&items=3&page=1&token=tmy1t6qddm2oqp4vdru8i58khd5ig6qvgobfh2e1"
-pickle_file_for_general_news = 'general.pickle'
+pickle_file_for_general_news = 'pickles/general.pickle'
 
-call_news_api(general_news_url, pickle_file_for_general_news) # run only once and comment out to avoid running into our api limits
+# call_news_api(general_news_url, pickle_file_for_general_news) # run only once and comment out to avoid running into our api limits
 add_news_for_return(pickle_file_for_general_news, 10)
 
-# SECTOR NEWS
-tech_news_url = f"https://stocknewsapi.com/api/v1/category?section=alltickers&items=3&page=1&token=tmy1t6qddm2oqp4vdru8i58khd5ig6qvgobfh2e1"
-pickle_file_for_tech_news = 'tech.pickle'
-# call_news_api(tech_news_url, pickle_file_for_tech_news)
 
-health_news_url = f"https://stocknewsapi.com/api/v1/category?section=alltickers&items=3&page=1&token=tmy1t6qddm2oqp4vdru8i58khd5ig6qvgobfh2e1&sector=healthcare"
-pickle_file_for_health_news = 'health.pickle'
-# call_news_api(health_news_url, pickle_file_for_health_news)
-
-fin_news_url = f"https://stocknewsapi.com/api/v1/category?section=alltickers&items=3&page=1&token=tmy1t6qddm2oqp4vdru8i58khd5ig6qvgobfh2e1&sector=financial"
-pickle_file_for_fin_news = 'fin.pickle'
-# call_news_api(fin_news_url, pickle_file_for_fin_news)
-
-basicmat_news_url = f"https://stocknewsapi.com/api/v1/category?section=alltickers&items=3&page=1&token=tmy1t6qddm2oqp4vdru8i58khd5ig6qvgobfh2e1&sector=basicmaterials"
-pickle_file_for_basicmat_news = 'basicmat.pickle'
-# call_news_api(basicmat_news_url, pickle_file_for_basicmat_news)
-
-
-industry_count = get_industries_in_portfolio(fake_portfolio)
-t, h, f, b = industry_count[0], industry_count[1], industry_count[2], industry_count[3]
-n = len(fake_portfolio)
-t, h, f, b = (t//n)*10, (h//n)*10, (f//n)*10, (b//n)*10
-
-add_news_for_return(pickle_file_for_tech_news, t)
-add_news_for_return(pickle_file_for_health_news, h)
-add_news_for_return(pickle_file_for_fin_news, f)
-add_news_for_return(pickle_file_for_basicmat_news, b)
-
-# STOCK NEWS, commented out to avoid running into api limits
+# STOCK NEWS
 tickers = get_tickers_in_portfolio(fake_portfolio)
 for ticker in tickers:
     ticker_url = f"https://stocknewsapi.com/api/v1?tickers={ticker}&items=3&page=1&token=tmy1t6qddm2oqp4vdru8i58khd5ig6qvgobfh2e1"
-    call_news_api(ticker_url, "some_ticker.pickle")
-    add_news_for_return("some_ticker.pickle", 3)
+    # call_news_api(ticker_url, "some_ticker.pickle")
+    add_news_for_return("pickles/some_ticker.pickle", 3)
 
 # shuffle results, and store them for persistence
 random.shuffle(news_to_return)
-with open("news_to_return.pickle", 'wb') as f:
+with open("pickles/news_to_return.pickle", 'wb') as f:
     pickle.dump(news_to_return, f)
     print("Final results saved to return news.pickle")
 
 # API function
 def get_custom_news():
     return news_to_return
-
 
