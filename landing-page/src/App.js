879d8f66fecf4ca64f00c420bac326092c6da55b
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-09-14 19:26:10 -0500

    Sean's commit -- FeedArticle display, updated app.js, added Tailwind

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import './index.css';
+// import logo from './logo.svg';
 import './App.css';
+import Newsfeed from './pages/Newsfeed';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Newsfeed />
     </div>
   );
 }
---
+++ landing-page/src/pages/Newsfeed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FeedArticle from '../components/FeedArticle'
+
+function Newsfeed() {
+    return (
+    <div>
+        <FeedArticle />
+    </div>
+    )
+}
+
+export default Newsfeed;
