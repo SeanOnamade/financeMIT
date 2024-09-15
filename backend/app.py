399dec4f875c59e37d719a76882c87d914ce8e01
Christopher Ojo <cojo@uwo.ca>
2024-09-14 21:43:43 -0400

    API ENDPOINT for ask Perplexity

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 def home():
 	return "This is the backend server for Pocket!"
 
-@app.route('/analyze/stock/<string:stock>')
+@app.route('/stock/<string:stock>')
 def show_post(stock):
     return get_stock_trend(stock)
 
+@app.route('/ask/<string:question>')
+def show_post(question):
+    return ask_perplexity(question)
 
 app.run(port=5000)
--- backend/prompting/prompt.py
+++ backend/prompting/prompt.py
@@ -56,7 +56,56 @@ def get_stock_trend(message: str) -> str:
     response = requests.post(url, json=payload, headers=headers)
     
     # Return the content of the response
-    print(response.text)
+    return response.json()
+
+def ask_perplexity(message: str) -> str:
+    """
+    Get the stock trend and related news from Perplexity API.
+    
+    Args:
+        message (str): The stock symbol to query.
+        
+    Returns:
+        str: The response content from the API.
+    """
+    
+    url = "https://api.perplexity.ai/chat/completions"
+
+    payload = {
+        "model": "llama-3.1-sonar-small-128k-online",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a finance stock trading expert. Be concise and informative."
+            },
+            {
+                "role": "user",
+                "content": message
+            }
+        ],
+        "max_tokens": 1000,
+        "temperature": 0.2,
+        "top_p": 0.9,
+        "return_citations": True,
+        "search_domain_filter": ["perplexity.ai"],
+        "return_images": False,
+        "return_related_questions": False,
+        "search_recency_filter": "month",
+        "top_k": 0,
+        "stream": False,
+        "presence_penalty": 0,
+        "frequency_penalty": 1
+    }
+    
+    headers = {
+        "Authorization": "Bearer " + PERPLEXITY_KEY,
+        "Content-Type": "application/json"
+    }
+    
+    # Make a POST request to the API
+    response = requests.post(url, json=payload, headers=headers)
+    
+    # Return the content of the response
     return response.json()
 
 # Example usage
